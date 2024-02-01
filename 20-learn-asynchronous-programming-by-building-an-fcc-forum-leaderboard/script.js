923b9ff5c0e87bd8e0bc9b8ce76af70932c392ab
VidelaR <videlaivan10@gmail.com>
2024-01-31 22:53:16 -0300

    Subo fcc forum leaderboard Step 1-67 TERMINADO

--- 20-learn-asynchronous-programming-by-building-an-fcc-forum-leaderboard/script.js
+++ 20-learn-asynchronous-programming-by-building-an-fcc-forum-leaderboard/script.js
@@ -5,6 +5,38 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+        299: { category: "Career Advice", className: "career" },
+        409: { category: "Project Feedback", className: "feedback" },
+        417: { category: "freeCodeCamp Support", className: "support" },
+        421: { category: "JavaScript", className: "javascript" },
+        423: { category: "HTML - CSS", className: "html-css" },
+        424: { category: "Python", className: "python" },
+        432: { category: "You Can Do This!", className: "motivation" },
+        560: { category: "Backend Development", className: "backend" },
+    };
+
+    const forumCategory = (id) => {
+    let selectedCategory = {};
+
+    if (allCategories.hasOwnProperty(id)) {
+        const { className, category } = allCategories[id];
+
+        selectedCategory.className = className;
+        selectedCategory.category = category;
+    } else {
+        selectedCategory.className = "general";
+        selectedCategory.category = "General";
+        selectedCategory.id = 1;
+    }
+    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+    const linkText = selectedCategory.category;
+    const linkClass = `category ${selectedCategory.className}`;
+
+    return `<a href="${url}" class="${linkClass}" target="_blank">
+        ${linkText}
+    </a>`;
+};
 
 const timeAgo = (time) => {
     const currentTime = new Date();
@@ -38,6 +70,21 @@ const viewCount = (views) => {
     return views;
 };
 
+const avatars = (posters, users) => {
+    return posters
+        .map((poster) => {
+        const user = users.find((user) => user.id === poster.user_id);
+        if (user) {
+            const avatar = user.avatar_template.replace(/{size}/, 30);
+            const userAvatarUrl = avatar.startsWith("/user_avatar/")
+            ? avatarUrl.concat(avatar)
+            : avatar;
+            return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+        }
+        })
+        .join("");
+};
+
 const fetchData = async () => {
     try {
         const res = await fetch(forumLatest);
@@ -46,7 +93,7 @@ const fetchData = async () => {
     } catch (err) {
         console.log(err);
     }
-};
+    };
 
 fetchData();
 
@@ -69,9 +116,14 @@ const showLatestPosts = (data) => {
         return `
         <tr>
             <td>
-                <p class="post-title">${title}</p>
+            <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
+                ${forumCategory(category_id)}
             </td>
-            <td></td>
+            <td>
+                <div class="avatar-container">
+                ${avatars(posters, users)}
+                </div>
+            </td>
             <td>${posts_count - 1}</td>
             <td>${viewCount(views)}</td>
             <td>${timeAgo(bumped_at)}</td>
